6c379ed888eb4aad2be234c8f137097749d6ff81
pratyushgupta1207 <pratyushgupta1207@gmail.com>
2025-10-01 07:04:56 +0100

    Added Calander 2025 SP PBL

---
+++ SP_PBL_CALENDER.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+int main() {
+    int year = 2025;
+    printf("Calendar for %d\n\n", year);
+
+    
+    printf("January\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("          1   2   3   4\n");
+    printf(" 5   6   7   8   9  10  11\n");
+    printf("12  13  14  15  16  17  18\n");
+    printf("19  20  21  22  23  24  25\n");
+    printf("26  27  28  29  30  31\n\n");
+
+    
+    printf("February\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("                      1\n");
+    printf(" 2   3   4   5   6   7   8\n");
+    printf(" 9  10  11  12  13  14  15\n");
+    printf("16  17  18  19  20  21  22\n");
+    printf("23  24  25  26  27  28\n\n");
+
+    printf("March\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("                      1\n");
+    printf(" 2   3   4   5   6   7   8\n");
+    printf(" 9  10  11  12  13  14  15\n");
+    printf("16  17  18  19  20  21  22\n");
+    printf("23  24  25  26  27  28  29\n");
+    printf("30  31\n\n");
+
+
+    printf("April\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("       1   2   3   4   5\n");
+    printf(" 6   7   8   9  10  11  12\n");
+    printf("13  14  15  16  17  18  19\n");
+    printf("20  21  22  23  24  25  26\n");
+    printf("27  28  29  30\n\n");
+
+    
+    printf("May\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("                1   2   3\n");
+    printf(" 4   5   6   7   8   9  10\n");
+    printf("11  12  13  14  15  16  17\n");
+    printf("18  19  20  21  22  23  24\n");
+    printf("25  26  27  28  29  30  31\n\n");
+
+    
+    printf("June\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf(" 1   2   3   4   5   6   7\n");
+    printf(" 8   9  10  11  12  13  14\n");
+    printf("15  16  17  18  19  20  21\n");
+    printf("22  23  24  25  26  27  28\n");
+    printf("29  30\n\n");
+
+    
+    printf("July\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("       1   2   3   4   5\n");
+    printf(" 6   7   8   9  10  11  12\n");
+    printf("13  14  15  16  17  18  19\n");
+    printf("20  21  22  23  24  25  26\n");
+    printf("27  28  29  30  31\n\n");
+
+
+    printf("August\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("                      1   2\n");
+    printf(" 3   4   5   6   7   8   9\n");
+    printf("10  11  12  13  14  15  16\n");
+    printf("17  18  19  20  21  22  23\n");
+    printf("24  25  26  27  28  29  30\n");
+    printf("31\n\n");
+
+    
+    printf("September\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("    1   2   3   4   5   6\n");
+    printf(" 7   8   9  10  11  12  13\n");
+    printf("14  15  16  17  18  19  20\n");
+    printf("21  22  23  24  25  26  27\n");
+    printf("28  29  30\n\n");
+
+    
+    printf("October\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("                1   2   3\n");
+    printf(" 4   5   6   7   8   9  10\n");
+    printf("11  12  13  14  15  16  17\n");
+    printf("18  19  20  21  22  23  24\n");
+    printf("25  26  27  28  29  30  31\n\n");
+
+    
+    printf("November\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf(" 1   2   3   4   5   6   7\n");
+    printf(" 8   9  10  11  12  13  14\n");
+    printf("15  16  17  18  19  20  21\n");
+    printf("22  23  24  25  26  27  28\n");
+    printf("29  30\n\n");
+
+    
+    printf("December\n");
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+    printf("    1   2   3   4   5   6\n");
+    printf(" 7   8   9  10  11  12  13\n");
+    printf("14  15  16  17  18  19  20\n");
+    printf("21  22  23  24  25  26  27\n");
+    printf("28  29  30  31\n\n");
+
+    return 0;
+}
